9b02bd6e0c7c76ac8524d8347c8324b427cab5e5
SanyFaysal <tfoysalahmed@gmail.com>
2024-03-06 17:57:47 +0600

    [DONE] added live server

--- src/redux/baseApi.js
+++ src/redux/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1",
+    baseUrl: "https://task-manager-server-xi-lemon.vercel.app/api/v1",
   }),
   tagTypes: ["Auth", "Todo"],
   endpoints: (builder) => ({}),
